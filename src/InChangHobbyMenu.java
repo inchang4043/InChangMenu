1e4bd98b7458f9b967dee67463ce08865ba464d8
inchang4043 <dlsckddld@naver.com>
2022-03-13 23:34:42 +0900

    removed Hello.java and added InChangHobbyMenu.java

---
+++ src/InChangHobbyMenu.java
@@ -0,0 +1,14 @@
+
+public class InChangHobbyMenu {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add Hobby");
+		System.out.println("1. Delete Hobby");
+		System.out.println("1. Edit Hobby");
+		System.out.println("1. View Hobby");
+		System.out.println("1. Exit Hobby");
+
+	
+	}
+
+}
