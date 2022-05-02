fbff14ed6a4a5d4ad6711df77106a2b2c5e2355c
inchang4043 <dlsckddld@naver.com>
2022-05-02 22:11:43 +0900

    updated X ManageMent

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,32 +1,32 @@
 import java.util.Scanner;
 
-public class FutsalMenu2 {
+public class MenuManager {
 
 	public static void main(String[] args) {
-		int num = 0;
-		Scanner  input = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
+		FutsalManager FutsalManager = new FutsalManager(input);
 		
-		while (num != 6) {
+		int num = -1;
+		while (num != 5) {
 		System.out.println("*** FutSal Management System Menu ***");
 		System.out.println("1. Add Member");
 		System.out.println("2. Delete Member");
 		System.out.println("3. Edit Member");
-		System.out.println("4. View Member");
-		System.out.println("5. Show a Menu");
-		System.out.println("6. Exit");
+		System.out.println("4. View Members");
+		System.out.println("5. Exit");
 		System.out.println("Select one number between 1 - 6 :");
 		num = input.nextInt();
 		if (num == 1) {
-			addMember();
+			FutsalManager.addMember();
 			}
 		else if (num == 2) {
-			deleteMember();
+			FutsalManager.deleteMember();
 			}
 		else if (num == 3) {
-			editMember();
+			FutsalManager.editMember();
 			}
 		else if (num == 4) {
-			viewMember();
+			FutsalManager.viewMembers();
 			}
 		else {
 			continue;
